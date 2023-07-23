a3e9efecbc2d7df8824b26392716505c4b73c216
Philipp Sommer <philipp.sommer@hereon.de>
2023-07-23 21:48:11 +0200

    minor fix for github api requests

--- static/js/github_api.js
+++ static/js/github_api.js
@@ -19,14 +19,18 @@ window.addEventListener("load", function() {
         }
       }
     ).then(
-      function (data) {
-        for  (var i = 0; i < data.length; i++) {
-          if (repos.indexOf(data[i].full_name) !== -1) {
-            x = data[i].name;
-            $("div[repo='" + x + "']").children(".star").html('<i class="fas fa-star"></i> ' + data[i].stargazers_count)
-            $("div[repo='" + x + "']").children(".fork").html('<i class="fas fa-code-fork"></i> ' + data[i].forks_count)
-          }
-        }
+      function (response) {
+        response.json().then(
+          (data) => data.forEach(
+            (repo) => {
+              if (repos.indexOf(repo.full_name) !== -1) {
+                x = repo.name;
+                $("div[repo='" + x + "']").children(".star").html('<i class="fas fa-star"></i> ' + repo.stargazers_count)
+                $("div[repo='" + x + "']").children(".fork").html('<i class="fas fa-code-fork"></i> ' + repo.forks_count)
+              }
+            }
+          )
+        )
       }
     )
   })
